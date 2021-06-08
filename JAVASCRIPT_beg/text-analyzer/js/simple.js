3da469bdbee06963dc8193849121222de7aedba1
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-08 16:25:36 -0700

    example added

--- JAVASCRIPT_beg/text-analyzer/js/simple.js
+++ JAVASCRIPT_beg/text-analyzer/js/simple.js
@@ -1,13 +1,4 @@
-console.log("js works");
-
-// Utility Logic
-
-function noInputtedWord(word, text) {
-  return ((text.trim().length === 0) || (word.trim().length === 0));
-}
-
 // Business Logic
-
 
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -22,9 +13,9 @@ function wordCounter(text) {
   });
   return wordCount;
 }
-console.log("wordCounter Runs")
+
 function numberOfOccurrencesInText(word, text) {
-  if (noInputtedWord(word, text)) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -39,35 +30,14 @@ function numberOfOccurrencesInText(word, text) {
 
 // UI Logic
 
-function boldPassage(word, text) {
-  if (noInputtedWord(word, text)) {
-    return "";
-  }
-  let htmlString = "<p>";
-  let textArray = text.split(" ");
-  textArray.forEach(function(element, index) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      htmlString = htmlString.concat("<b>" + element + "</b>");
-    } else {
-      htmlString = htmlString.concat(element);
-    }
-    if (index !== (textArray.length - 1)) {
-      htmlString = htmlString.concat(" ");
-    }
-  });
-  return htmlString + "</p>";
-}
-
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
     event.preventDefault();
-    console.log("function runs!");
     const passage = $("#text-passage").val();
     const word = $("#word").val();
     const wordCount = wordCounter(passage);
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
-    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
