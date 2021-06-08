d81f05e3b0eb338dd695ab701a21526486d9825b
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-08 14:35:26 -0700

    move function order in bolder.js

--- JAVASCRIPT_beg/text-analyzer/README.md
+++ JAVASCRIPT_beg/text-analyzer/README.md
@@ -11,3 +11,10 @@ Code:
 const text = "hello there";
 wordCounter(text);
 Expected Output: 2
+
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
--- JAVASCRIPT_beg/text-analyzer/js/bolder.js
+++ JAVASCRIPT_beg/text-analyzer/js/bolder.js
@@ -1,3 +1,17 @@
+console.log("js works");
+
+$(document).ready(function(){
+  $("form#word-bolder").submit(function(event){
+    event.preventDefault();
+    console.log("submit works")
+    const text = $("#text-passage").val();
+    const word = $("#word").val();
+    const boldedPassage = boldPassage(text);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#boldedPassage").html(boldPassage);
+  });
+});
+
 function boldPassage(word, text) {
   let htmlString = "<p>";
   let textArray = text.split(" ");
@@ -10,18 +24,5 @@ function boldPassage(word, text) {
     htmlString = htmlString.concat(" ");
   });
   return htmlString + "</p>";
-  console.log("boldPassage works!");
 }
 
-$(document).ready(function(){
-  $("form#word-bolder").submit(function(event){
-    event.preventDefault();
-    console.log("submit works")
-    const text = $("#text-passage").val();
-    const word = $("#word").val();
-    const boldedPassage = boldPassage(text);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-    $("#total-count").html(wordCount);
-    $("#boldedPassage").html(boldPassage);
-  });
-});
