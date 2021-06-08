5c5ea4345d56e812ed6f352815d484faca6b9de1
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-08 15:31:58 -0700

    add updated js to bolder.js

--- JAVASCRIPT_beg/text-analyzer/js/bolder.js
+++ JAVASCRIPT_beg/text-analyzer/js/bolder.js
@@ -1,28 +1,72 @@
 console.log("js works");
 
-$(document).ready(function(){
-  $("form#word-bolder").submit(function(event){
-    event.preventDefault();
-    console.log("submit works")
-    const text = $("#text-passage").val();
-    const word = $("#word").val();
-    const boldedPassage = boldPassage(text);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-    $("#boldedPassage").html(boldPassage);
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
+// Business Logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
   });
-});
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  if (noInputtedWord(word, text)) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+// UI Logic
 
 function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
   let htmlString = "<p>";
   let textArray = text.split(" ");
-  textArray.forEach(function(element) {
-    if (word === element) {
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
       htmlString = htmlString.concat("<b>" + element + "</b>");
     } else {
       htmlString = htmlString.concat(element);
     }
-    htmlString = htmlString.concat(" ");
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
   });
   return htmlString + "</p>";
 }
 
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    console.log("function runs!");
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
+  });
+});
